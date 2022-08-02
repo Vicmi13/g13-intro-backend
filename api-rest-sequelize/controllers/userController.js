50919bd7d22a71388c035e9e543bfc7d6d121425
Victor Torres Valencia <victor.tv@wizeline.com>
2022-08-01 19:21:16 -0500

    Added console in createUser method

--- api-rest-sequelize/controllers/userController.js
+++ api-rest-sequelize/controllers/userController.js
@@ -5,7 +5,11 @@ const User = require("../models/user");
 
 const getAllUsers = async (_, res) => {
   try {
-    const result = await User(sequelize, DataTypes).findAll();
+    /**1ra forma de implementacion */
+    // const result = await User(sequelize, DataTypes).findAll();
+
+    /**2da forma de implementacion */
+    const result = await User.findAll();
     console.log("user results", result);
     res.status(200).json({
       message: "Recover all users",
@@ -20,6 +24,7 @@ const createUser = async (req, res) => {
   const { firstName, lastName, email } = req.body;
   try {
     const result = await User.create({ firstName, lastName, email });
+    console.log("result", result.dataValues);
     res.status(201).json({
       message: "User created successfully",
       data: result,
